60c014dcb2937845bed7fc460ba26f64efb3efaf
Jonathan <farinloyejonathan@gmail.com>
2022-01-06 09:20:48 +0100

    added password hashing

--- account/functions.go
+++ account/functions.go
@@ -1,10 +1,15 @@
 package account
 
 import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"golang.org/x/crypto/argon2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -32,4 +37,50 @@ func InitDatbase() *gorm.DB {
 	}
 
 	return db
+}
+
+func generatePasswordHash(password string) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	config := &passwordConfig{
+		time:    1,
+		memory:  64 * 1024,
+		threads: 4,
+		keyLen:  32,
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, config.time, config.memory, config, threads, config.keyLen)
+	b64Salt := base64.RawStdEncoding(salt)
+	b64Hash := base64.RawStdEncoding(hash)
+
+	format := "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s"
+	full := fmt.Sprintf(format, argon2.version, config.memory, config.time, config.threads, b64Salt, b64Hash)
+
+	return full, nil
+}
+
+func comparePassword(password, hash string) (bool, error) {
+	parts := strings.Split(hash, "$")
+	config := &passwordConfig{}
+
+	_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &config.memory, &config.time, &config.threads)
+	if err != nil {
+		return false, err
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+	if err != nil {
+		return false, err
+	}
+	decodedHash, err := base64.RawStdEncoding.DecodeString(parts[5])
+	if err != nil {
+		return false, err
+	}
+
+	config.keyLen = uint32(len(hash))
+	comparinsonHash := argon2.IDKey([]byte(password), salt, config.time, config.memory, config.threads, config.keyLen)
+	return subtle.ConstantTimeCompare(decodedHash, comaprisonHash) == 1, nil
 }
--- account/model.go
+++ account/model.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID          uint      `json:"id"`
 	FullName    string    `json;"full_name"`
+	Username    string    `json:"username"`
 	PhoneNumber string    `json:"phone_number"`
 	Password    string    `json:"password"`
 	CreatedAt   time.Time `json:"created_at"`
