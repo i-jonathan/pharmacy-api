ec5739e4d9c5aa50f33bff44a6218cf04427cd79
Jonathan <farinloyejonathan@gmail.com>
2022-05-19 16:46:09 +0100

    feat(sql): role related sql interactions.

--- connection/db/sql/role.go
+++ connection/db/sql/role.go
@@ -1,23 +1,100 @@
 package db
 
-import "github.com/i-jonathan/pharmacy-api/model"
+import (
+	"database/sql"
+	appError "github.com/i-jonathan/pharmacy-api/error"
+	"github.com/i-jonathan/pharmacy-api/model"
+	"log"
+)
 
 func (r *repo) FetchRoles() ([]model.Role, error) {
-	panic("implement me")
+	var result []model.Role
+	const statement = "SELECT id, name, created_at FROM role;"
+
+	rows, err := r.Conn.Query(statement)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var temp model.Role
+		err = rows.Scan(&temp.ID, &temp.Name, &temp.CreatedAt)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		temp.Slug, err = model.ToHashID(temp.ID)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		result = append(result, temp)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+	}
+
+	return result, nil
 }
 
 func (r *repo) FetchRoleByID(id int) (model.Role, error) {
-	panic("implement me")
+	var result model.Role
+	const statement = "SELECT id, name, created_at FROM role WHERE id = $1;"
+	row := r.Conn.QueryRow(statement, id)
+	
+	var err error
+	if err = row.Err(); row != nil {
+		return model.Role{}, err
+	}
+	
+	err = row.Scan(&result.ID, &result.Name, &result.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return model.Role{}, appError.NotFound
+		}
+		return model.Role{}, err
+	}
+	result.Slug, err = model.ToHashID(result.ID)
+	if err != nil {
+		log.Println(err)
+	}
+	return result, nil
+
 }
 
 func (r *repo) CreateRole(role model.Role) (int, error) {
-	panic("implement me")
+	var id int
+	const statement = "INSERT INTO role (name, description) VALUES ($1, $2) returning id;"
+
+	err := r.Conn.QueryRow(statement, role.Name, role.Description).Scan(&id)
+
+	if err != nil || id < 1 {
+		return 0, err
+	}
+
+	return id, nil
 }
 
 func (r *repo) UpdateRole(role model.Role) error {
-	panic("implement me")
+	const statement = "UPDATE role SET name = $1, description = $2 WHERE id = $3;"
+
+	_, err := r.Conn.Exec(statement, role.Name, role.Description, role.ID)
+	if err != nil {
+		return err
+	}
+
+	return  nil
 }
 
 func (r *repo) DeleteRole(id int) error {
-	panic("implement me")
+	const statement = "DELETE FROM role WHERE id = $1;"
+
+	_, err := r.Conn.Exec(statement, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- model/account.go
+++ model/account.go
@@ -13,6 +13,7 @@ type Permission struct {
 type Role struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
+	Description string	`json:"description"`
 	CreatedAt time.Time `json:"created_at"`
 	Permissions []Permission `json:"permissions"`
 	Slug      string    `json:"slug"`
