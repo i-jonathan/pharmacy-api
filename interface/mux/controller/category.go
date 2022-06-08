4f91aecd385dd6fc01c96c661dd102d72adabebc
Jonathan <farinloyejonathan@gmail.com>
2022-06-08 10:45:49 +0100

    feat: category controllers complete

--- interface/mux/controller/category.go
+++ interface/mux/controller/category.go
@@ -15,7 +15,7 @@ type categoryController struct {
 	svc service.CategoryUseCase
 }
 
-func NewServiceController(s service.CategoryUseCase) *categoryController {
+func NewCategoryController(s service.CategoryUseCase) *categoryController {
 	return &categoryController{s}
 }
 
@@ -37,7 +37,7 @@ func (controller *categoryController) FetchCategories(w http.ResponseWriter, r *
 	helper.ReturnSuccess(w, result)
 }
 
-func (controller *categoryController) FetchControllerBySlug(w http.ResponseWriter, r *http.Request) {
+func (controller *categoryController) FetchCategoryBySlug(w http.ResponseWriter, r *http.Request) {
 	const perm = "category:read"
 	allowed, err := model.CheckPermission(perm, r)
 	if !allowed {
@@ -112,5 +112,20 @@ func (controller *categoryController) UpdateCategory(w http.ResponseWriter, r *h
 }
 
 func (controller *categoryController) DeleteCategory(w http.ResponseWriter, r *http.Request) {
-	// TODO continue from here
+	const perm = "category:delete"
+	allowed, err := model.CheckPermission(perm, r)
+	if !allowed {
+		log.Println(err)
+		helper.ReturnFailure(w, appError.Forbidden)
+		return
+	}
+
+	slug := mux.Vars(r)["slug"]
+	err = controller.svc.DeleteCategory(slug)
+	if err != nil {
+		helper.ReturnFailure(w, err)
+		return
+	}
+
+	helper.ReturnEmptyBody(w, http.StatusNoContent)
 }
